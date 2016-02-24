183a6fb581b68dff442dbecb79f7d9769fa151da
gpelipas <jon.pelipas@gmail.com>
2016-02-24 17:45:00 -0500

    Create Details.js

---
+++ src/main/webapp/app/product/model/Details.js
@@ -0,0 +1,8 @@
+Ext.define('Test.product.model.Detail', {
+    extend: 'Ext.data.Model',
+    fields: [
+        {name: 'code', type: 'string'},             
+        {name: 'value',  type: 'string'} 
+    ],
+    belongsTo: 'Test.product.model.Product'
+});
